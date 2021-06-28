de4b8c1df024b0a1a6cfae5ed82b932442d829b9
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2021-06-28 14:21:55 +0530

    Add devtools

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { QueryClientProvider, QueryClient } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 function MyApp({ Component, pageProps }) {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
       <Component {...pageProps} />
     </QueryClientProvider>
   );
