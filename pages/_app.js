007c6e72784b0ccbced38edfdab1b8c80dfc56bb
Dhaiwat Pandya <dhaiwatpandya@gmail.com>
2021-06-28 18:01:51 +0530

    fix missing devtools

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
